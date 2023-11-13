4620e212fcfb710e187fa5a8cdcc3e201c1c8dbb
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-11-13 17:50:11 -0300

    Properly check for OpenWeatherMap API response errors

--- src/blocks/weather/index.php
+++ src/blocks/weather/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Server-side rendering of the `fleximple-block/weather` block.
  *
@@ -12,7 +13,8 @@
  *
  * @return string Returns the current date & time according to the settings chosen.
  */
-function fleximpleblocks_render_weather_block( $attributes ) {
+function fleximpleblocks_render_weather_block($attributes)
+{
   $default_class_name = 'fleximple-block-weather';
   // $class_name = $attributes['className'];
   // $classes = 'fleximple-block-weather';
@@ -22,7 +24,7 @@ function fleximpleblocks_render_weather_block( $attributes ) {
    */
   $weather_markup = ''; // It is important to initiate it here so nothing will be overridden by the following code.
 
-  if ( isset( $attributes['cityName'] ) && $attributes['cityName'] ) {
+  if (isset($attributes['cityName']) && $attributes['cityName']) {
     $city_name = $attributes['cityName'];
     $short_name =  $attributes['shortName'];
     $api_key = get_option('fleximpleblocks_open_weather_api_key');
@@ -30,22 +32,25 @@ function fleximpleblocks_render_weather_block( $attributes ) {
     $units = $attributes['units'];
     $language = $attributes['language'];
 
-    $url = 'https://api.openweathermap.org/data/2.5/weather?q='. $city_name .'&appid='. $api_key .'&units='. $units .'&lang='. $language;
+    $url = 'https://api.openweathermap.org/data/2.5/weather?q=' . $city_name . '&appid=' . $api_key . '&units=' . $units . '&lang=' . $language;
 
-    $response = wp_remote_get( $url );
-    if ( $response && $response['response'] && $response['response']['code'] === 200 ) {
-      $weather_data = json_decode( $response['body'] );
+    $response = wp_remote_get($url);
+    $weather_data = null;
+    if (!is_wp_error($response) && $response['response'] && $response['response']['code'] === 200) {
+      $weather_data = json_decode($response['body']);
     }
 
     $time_stamp = $weather_data->dt;
     $sunrise = $weather_data->sys->sunrise;
     $sunset = $weather_data->sys->sunset;
-    if ( ( $time_stamp > $sunrise ) && ( $time_stamp < $sunset ) ) {
+    if (($time_stamp > $sunrise) && ($time_stamp < $sunset)) {
       $time_of_day = 'day';
-    } else { $time_of_day = 'night'; }
+    } else {
+      $time_of_day = 'night';
+    }
 
     $units_initial = '';
-    switch( $units ) {
+    switch ($units) {
       case 'metric':
         $units_initial = 'C';
         break;
@@ -60,32 +65,32 @@ function fleximpleblocks_render_weather_block( $attributes ) {
         break;
     }
 
-    $weather_markup = '<i class="'. $default_class_name .'__icon wi wi-owm-'. $time_of_day .'-'. $weather_data->weather[0]->id .'"></i>';
-    $weather_markup .= '<div class="'. $default_class_name .'__temperature">';
-    $weather_markup .= '<span class="'. $default_class_name .'__temperature_number">'. round( $weather_data->main->temp ) .'</span>';
-    $weather_markup .= '<span class="'. $default_class_name .'__temperature_degree-symbol">&deg;</span>';
-    if ( $display_units ) {
-      $weather_markup .= '<span class="'. $default_class_name .'__temperature_units">'. $units_initial .'</span>';
+    $weather_markup = '<i class="' . $default_class_name . '__icon wi wi-owm-' . $time_of_day . '-' . $weather_data->weather[0]->id . '"></i>';
+    $weather_markup .= '<div class="' . $default_class_name . '__temperature">';
+    $weather_markup .= '<span class="' . $default_class_name . '__temperature_number">' . round($weather_data->main->temp) . '</span>';
+    $weather_markup .= '<span class="' . $default_class_name . '__temperature_degree-symbol">&deg;</span>';
+    if ($display_units) {
+      $weather_markup .= '<span class="' . $default_class_name . '__temperature_units">' . $units_initial . '</span>';
     }
     $weather_markup .= '</div>';
-    $weather_markup .= '<span class="'. $default_class_name .'__short-name">'. $short_name .'</span>';
-    $weather_markup .= '<span class="'. $default_class_name .'__city-name">'. $weather_data->name .'</span>';
+    $weather_markup .= '<span class="' . $default_class_name . '__short-name">' . $short_name . '</span>';
+    $weather_markup .= '<span class="' . $default_class_name . '__city-name">' . $weather_data->name . '</span>';
   }
 
   $internal_styles = '';
-  if ( isset( $attributes['shortName'] ) && $attributes['shortName'] ) {
+  if (isset($attributes['shortName']) && $attributes['shortName']) {
     $internal_styles .= '<style>';
-    $internal_styles .= '.'. $default_class_name .'__city-name { display: none; }
-    @media only screen and (min-width: '. get_option('fleximpleblocks_xlarge_breakpoint_value') .'px) {
-      .'. $default_class_name .'__short-name { display: none; }
-      .'. $default_class_name .'__city-name { display: inline-block; }
+    $internal_styles .= '.' . $default_class_name . '__city-name { display: none; }
+    @media only screen and (min-width: ' . get_option('fleximpleblocks_xlarge_breakpoint_value') . 'px) {
+      .' . $default_class_name . '__short-name { display: none; }
+      .' . $default_class_name . '__city-name { display: inline-block; }
     }';
     $internal_styles .= '</style>';
   }
 
   $block_content = sprintf(
     '<div class="%s">%s%s</div>',
-    esc_attr( $default_class_name ),
+    esc_attr($default_class_name),
     $internal_styles,
     $weather_markup
   );
@@ -96,10 +101,11 @@ function fleximpleblocks_render_weather_block( $attributes ) {
 /**
  * Registers the `fleximple-block/weather` block.
  */
-if ( ! function_exists( 'fleximpleblocks_register_weather_block' ) ) {
-  function fleximpleblocks_register_weather_block() {
+if (!function_exists('fleximpleblocks_register_weather_block')) {
+  function fleximpleblocks_register_weather_block()
+  {
     // Return early if this function does not exist.
-    if ( ! function_exists( 'register_block_type_from_metadata' ) ) {
+    if (!function_exists('register_block_type_from_metadata')) {
       return;
     }
 
@@ -110,5 +116,5 @@ if ( ! function_exists( 'fleximpleblocks_register_weather_block' ) ) {
       )
     );
   }
-  add_action( 'init', 'fleximpleblocks_register_weather_block' );
+  add_action('init', 'fleximpleblocks_register_weather_block');
 }
